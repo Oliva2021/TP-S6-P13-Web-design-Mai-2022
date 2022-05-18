75f0b5a26525ba62858866af4be4476ae2b9b89a
Your Name <you@example.com>
2022-05-18 19:47:30 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,268 @@
+<!--
+Author: WebThemez
+Author URL: http://webthemez.com
+License: Creative Commons Attribution 3.0 Unported
+License URL: http://creativecommons.org/licenses/by/3.0/
+-->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="description" content="Real Estate Builders Free Responsive Website Templates - icon">
+	<meta name="author" content="webThemez.com">
+	<title>Real Estate Builders Free Responsive Website - icon</title>
+	<link rel="favicon" href="assets/images/favicon.png">
+	<link rel="stylesheet" media="screen" href="http://fonts.googleapis.com/css?family=Open+Sans:300,400,700">
+	<link rel="stylesheet" href="assets/css/bootstrap.min.css">
+	<link rel="stylesheet" href="assets/css/font-awesome.min.css"> 
+	<link rel="stylesheet" href="assets/css/bootstrap-theme.css" media="screen"> 
+	<link rel="stylesheet" href="assets/css/style.css">
+    <link rel='stylesheet' id='camera-css'  href='assets/css/camera.css' type='text/css' media='all'> 
+	<!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+	<!--[if lt IE 9]>
+	<script src="assets/js/html5shiv.js"></script>
+	<script src="assets/js/respond.min.js"></script>
+	<![endif]-->
+</head>
+<body>
+	<!-- Fixed navbar -->
+	<div class="navbar navbar-inverse">
+		<div class="container">
+			<div class="navbar-header">
+				<!-- Button for smallest screens -->
+				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse"><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button>
+				<a class="navbar-brand" href="index.html">
+					<img src="assets/images/logo.png" alt="Techro HTML5 template"></a>
+			</div>
+			<div class="navbar-collapse collapse">
+				<ul class="nav navbar-nav pull-right mainNav">
+					<li class="active"><a href="index.html">Home</a></li>
+					<li><a href="about.html">About</a></li>
+					<li><a href="services.html">Services</a></li>
+					<li><a href="price.html">Price</a></li>
+					<li><a href="projects.html">Projects</a></li>
+					<li class="dropdown">
+						<a href="#" class="dropdown-toggle" data-toggle="dropdown">Pages <b class="caret"></b></a>
+						<ul class="dropdown-menu">
+							<li><a href="sidebar-right.html">Right Sidebar</a></li>
+							<li><a href="#">Dummy Link1</a></li>
+							<li><a href="#">Dummy Link2</a></li>
+							<li><a href="#">Dummy Link3</a></li>
+						</ul>
+					</li>
+					<li><a href="home.php">LOGIN</a></li>
+
+				</ul>
+			</div>
+			<!--/.nav-collapse -->
+		</div>
+	</div>
+	<!-- /.navbar -->
+
+	<!-- Header -->
+	<header id="head">
+		<div class="container">
+					<div class="fluid_container">
+                    <div class="camera_wrap camera_emboss pattern_1" id="camera_wrap_4">
+                        <div data-thumb="assets/images/slides/thumbs/slide1.jpg" data-src="assets/images/slides/slide1.jpg">
+                        </div> 
+                        <div data-thumb="assets/images/slides/thumbs/slide2.jpg" data-src="assets/images/slides/slide2.jpg">
+                        </div>
+                        <div data-thumb="assets/images/slides/thumbs/slide3.jpg" data-src="assets/images/slides/slide3.jpg">
+                        </div> 
+                    </div><!-- #camera_wrap_3 -->
+                </div><!-- .fluid_container -->
+		</div>
+	</header>
+	<!-- /Header -->
+
+      <section class="secpadding">
+	  <div class="container">
+      <div class="row">
+      	<div class="col-md-12"><div class="title-box clearfix "><h2 class="title-box_primary">
+              Définition simple du réchauffement climatique</h2></div> 
+           <p>Le réchauffement climatique est un phénomène global de transformation du climat caractérisé par une augmentation générale des températures moyennes (notamment liée aux activités humaines), et qui modifie durablement les équilibres météorologiques et les écosystèmes.
+            Lorsque l’on en parle aujourd’hui, il s’agit du phénomène d’augmentation des températures qui se produit sur Terre depuis 100 à 150 ans. Depuis le début de la Révolution Industrielle, les températures moyennes sur terre ont en effet augmenté plus ou moins régulièrement. En 2016, la température moyenne sur la planète terre était environ 1 à 1.5 degrés au dessus des températures moyennes de l’ère pré-industrielle (avant 1850). </p>
+		</div>
+        </div>
+		</div>
+		</section>
+      <section class="news-box">
+        <div class="container">
+            <h2><span>Les causes du réchauffement climatique</span></h2>
+            <div class="row">
+
+                    <div class="col-md-4 col-sm-6 col-xs-12">
+                        <div class="thumbnail thumbnail4">
+                            <img src="assets/images/slides/slide1.jpg" alt="">
+
+                            <div class="caption">
+                                <h4>
+                                    <a href="#">
+                                        
+                                    </a>
+                                </h4>
+
+                                <p>
+                                En cause notamment, la production d’énergie pour 
+                                l’électricité ou le chauffage qui nécessite l’exploitation 
+                                de charbon ou de pétrole, ainsi que les transports de personnes et 
+                                de marchandises. Les voitures, l’aviation et le transport maritime rejettent 
+                                14% des gaz à effet de serre mondiaux responsables du réchauffement climatique.
+                                </p>
+                                <a href="#" data-title="Read More" class="btn-link"><span>Read More</span></a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4 col-sm-6 col-xs-12">
+                        <div class="thumbnail thumbnail4">
+                            <img src="assets/images/slides/slide2.jpg" alt="">
+
+                            <div class="caption">
+                                <h4>
+                                    <a href="#">
+                                        
+                                    </a>
+                                </h4>
+
+                                <p>
+                                Viennent ensuite la déforestation, l’agriculture et l’élevage. La production de 
+                                viande et de produits laitiers émet la moitié des gaz à effet de serre liés à
+                                 l’alimentation dans le monde. En outre, agriculture et déforestation sont étroitement 
+                                 liées puisque la majorité des forêts récupérées par l’Homme serviront aux pâturages 
+                                 et à la production de céréales spécifiquement dédiées au bétail. Des céréales qui suffiraient 
+                                autrement à nourrir plus de trois milliards de personnes. 
+                                </p>
+                                <a href="#" data-title="Read More" class="btn-link"><span>Read More</span></a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4 col-sm-6 col-xs-12">
+                        <div class="thumbnail thumbnail4">
+                            <img src="assets/images/slides/slide3.jpg" alt="">
+
+                            <div class="caption">
+                                <h4>
+                                    <a href="#">
+                                        
+                                    </a>
+                                </h4>
+
+                                <p>
+                                Alors bien évidemment, la température moyenne du globe varie d’une année à l’autre  mais la
+                                 tendance ne peut en aucun cas être confondue avec le problème de fond qui nous concerne. 
+                                 Depuis le siècle dernier, la planète s’est globalement réchauffée de 0,9 degrés, 2016 ayant 
+                                 été l’année la plus chaude jamais enregistrée avec des températures 1 à 1,5 degrés 
+                                supérieures par rapport à celles de l’ère pré-industrielle, soit avant 1850. 
+                                </p>
+                                <a href="#" data-title="Read More" class="btn-link"><span>Read More</span></a>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+        </div>
+    </section>
+   
+ 
+	
+      <section class="btm-section secpadding">
+	  <div class="container">
+      <div class="row">
+      	    <div class="col-md-4"><div class="title-box clearfix "><h3 class="title-box_primary">New Projects</h3></div> 
+            <div class="list styled custom-list">
+            <ul>
+            <li><a title="Snatoque penatibus et magnis dis partu rient montes ascetur ridiculus mus." href="#">Singapore Township</a></li>
+            <li><a title="Fusce feugiat malesuada odio. Morbi nunc odio gravida at cursus nec luctus." href="#">Mega luxury Villas</a></li>
+            <li><a title="Penatibus et magnis dis parturient montes ascetur ridiculus mus." href="#">RNT Commercial Shopping Mall</a></li>
+            <li><a title="Morbi nunc odio gravida at cursus nec luctus a lorem. Maecenas tristique orci." href="#">SVN Independent & Duplex Houses</a></li>
+            <li><a title="Snatoque penatibus et magnis dis partu rient montes ascetur ridiculus mus." href="#">World wide IT park</a></li>
+            <li><a title="Fusce feugiat malesuada odio. Morbi nunc odio gravida at cursus nec luctus." href="#">North Arena SNT Township</a></li>
+            </ul>
+            </div>
+         </div>
+        
+        <div class="col-md-4"><div class="title-box clearfix "><h3 class="title-box_primary">Large Projects</h3></div> 
+            <div class="list styled custom-list">
+            <ul>
+            <li><a title="Snatoque penatibus et magnis dis partu rient montes ascetur ridiculus mus." href="#">Singapore Township</a></li>
+            <li><a title="Fusce feugiat malesuada odio. Morbi nunc odio gravida at cursus nec luctus." href="#">Mega luxury Villas</a></li>
+            <li><a title="Penatibus et magnis dis parturient montes ascetur ridiculus mus." href="#">RNT Commercial Shopping Mall</a></li>
+            <li><a title="Morbi nunc odio gravida at cursus nec luctus a lorem. Maecenas tristique orci." href="#">SVN Independent & Duplex Houses</a></li>
+            <li><a title="Snatoque penatibus et magnis dis partu rient montes ascetur ridiculus mus." href="#">World wide IT park</a></li>
+            <li><a title="Fusce feugiat malesuada odio. Morbi nunc odio gravida at cursus nec luctus." href="#">North Arena SNT Township</a></li>
+            </ul>
+            </div>
+         </div>
+         <div class="col-md-4"><div class="title-box clearfix "><h3 class="title-box_primary">Our Clients</h3></div> 
+            <blockquote class="blockquote-1">
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante. Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid</p>
+					<small>Someone famous in <cite title="Source Title">Source Title</cite></small>
+				</blockquote></div>
+         
+      </div></div>
+      </section>
+      
+    	 
+    <footer id="footer">
+	 
+		<div class="footer2">
+			<div class="container">
+				<div class="row">
+
+					<div class="col-md-6 panel">
+						<div class="panel-body">
+							<p class="simplenav">
+								<a href="index.html">Home</a> | 
+								<a href="about.html">About</a> |
+								<a href="services.html">Services</a> |
+								<a href="price.html">Price</a> |
+								<a href="projects.html">Projects</a> |
+								<a href="contact.html">Contact</a>
+							</p>
+						</div>
+					</div>
+
+					<div class="col-md-6 panel">
+						<div class="panel-body">
+							<p class="text-right">
+								Copyright &copy; 2015. Template by <a href="http://webthemez.com/" rel="develop">WebThemez.com</a>
+							</p>
+						</div>
+					</div>
+
+				</div>
+				<!-- /row of panels -->
+			</div>
+		</div>
+	</footer>
+
+	<!-- JavaScript libs are placed at the end of the document so the pages load faster -->
+	<script src="assets/js/modernizr-latest.js"></script> 
+	<script type='text/javascript' src='assets/js/jquery.min.js'></script>
+    <script type='text/javascript' src='assets/js/fancybox/jquery.fancybox.pack.js'></script>
+    
+    <script type='text/javascript' src='assets/js/jquery.mobile.customized.min.js'></script>
+    <script type='text/javascript' src='assets/js/jquery.easing.1.3.js'></script> 
+    <script type='text/javascript' src='assets/js/camera.min.js'></script> 
+    <script src="assets/js/bootstrap.min.js"></script> 
+	<script src="assets/js/custom.js"></script>
+    <script>
+		jQuery(function(){
+			
+			jQuery('#camera_wrap_4').camera({
+				height: '750',
+				loader: 'bar',
+				pagination: false,
+				thumbnails: false,
+				hover: false,
+				opacityOnGrid: false,
+				imagePath: 'assets/images/'
+			});
+
+		});
+	</script>
+    
+</body>
+</html>
